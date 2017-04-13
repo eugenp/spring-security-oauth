5107cac1cfeedcc18344a707bbe96d118375ac45
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2017-04-13 21:53:53 +0300

    fix conflict

--- spring-security-oauth-server/src/main/java/org/baeldung/config/TokenController.java
+++ spring-security-oauth-server/src/main/java/org/baeldung/config/TokenController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class TokenController {
@@ -32,6 +33,7 @@ public class TokenController {
             String tokenId = authorization.substring("Bearer".length()+1);
             tokenServices.revokeToken(tokenId);
         }
+	}
 
     @RequestMapping(method = RequestMethod.GET, value = "/tokens")
     @ResponseBody
