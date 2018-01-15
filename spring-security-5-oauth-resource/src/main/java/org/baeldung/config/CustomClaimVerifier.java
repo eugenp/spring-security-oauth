b2f0681ff7e2fed1967709d37ec0ee91a6732d13
Oleksandr Kolodiazhnyi <a.kolodiazhny@gmail.com>
2018-01-15 10:09:21 +0200

    minor fixes in if conditions of cors flter and customer claim verifier

--- spring-security-5-oauth-resource/src/main/java/org/baeldung/config/CustomClaimVerifier.java
+++ spring-security-5-oauth-resource/src/main/java/org/baeldung/config/CustomClaimVerifier.java
@@ -1,15 +1,16 @@
 package org.baeldung.config;
 
-import java.util.Map;
-
 import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.provider.token.store.JwtClaimsSetVerifier;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 public class CustomClaimVerifier implements JwtClaimsSetVerifier {
     @Override
     public void verify(Map<String, Object> claims) throws InvalidTokenException {
         final String username = (String) claims.get("user_name");
-        if ((username == null) || (username.length() == 0)) {
+        if (StringUtils.isEmpty(username)) {
             throw new InvalidTokenException("user_name claim is empty");
         }
     }
--- spring-security-oauth-resource/src/main/java/org/baeldung/config/CorsFilter.java
+++ spring-security-oauth-resource/src/main/java/org/baeldung/config/CorsFilter.java
@@ -1,19 +1,14 @@
 package org.baeldung.config;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -26,7 +21,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Max-Age", "3600");
-        if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
             chain.doFilter(req, res);
