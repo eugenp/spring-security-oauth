8b39de4385c022705b7f47adf3c71bf6c3222f3d
GAUTAM\Gautam <gautam.rs.yadav@gmail.com>
2018-01-23 14:22:02 +0530

    New test cases added

--- spring-security-5-oauth-resource/src/main/java/org/baeldung/config/ResourceServerApplication.java
+++ spring-security-5-oauth-resource/src/main/java/org/baeldung/config/ResourceServerApplication.java
@@ -2,9 +2,15 @@ package org.baeldung.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@Configuration
+@EnableWebMvc
+@ComponentScan({ "org.baeldung.web.controller" })
 public class ResourceServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
